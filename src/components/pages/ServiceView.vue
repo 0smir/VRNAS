43458aa37bf73449094676335ecb40b04b434238
o.smirnova <o.v.smirnova1987@gmail.com>
2025-09-26 19:47:35 +0300

    add Service page view

---
+++ src/components/pages/ServiceView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        Service page
+        <div loader>
+            <loader />
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -2,10 +2,12 @@ import { createMemoryHistory, createRouter } from 'vue-router';
 
 import HomeView from '@/components/pages/HomeView.vue'
 import AboutView from '@/components/pages/AboutView.vue'
+import ServiceView from '@/components/pages/ServiceView.vue'
 
 const routes = [
   { path: '/', component: HomeView },
-  { path: '/about', component: AboutView }
+  { path: '/about', component: AboutView },
+  { path: '/service', component: ServiceView }
 ]
 
 const router = createRouter({
