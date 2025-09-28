7bb426bd9f4ad7f5463056d5b82aca94074e9b4a
o.smirnova <o.v.smirnova1987@gmail.com>
2025-09-28 22:53:34 +0300

    import of vue-i18n

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
-import { createApp } from 'vue'
-import router from '@/router'
-import App from './App.vue'
+import { createApp } from 'vue';
+import router from '@/router';
+import App from './App.vue';
+import { createI18n } from 'vue-i18n';
 
-const app = createApp(App)
+// imports of translations
+import de from '@/locales/de.json';
+import en from '@/locales/en.json';
+
+// configure i18n
+const i18n = createI18n({
+  locale: 'en',
+  fallbackLocale: 'en',
+  messages: { de, en },
+});
+
+const app = createApp(App);
 
 import BaseButton from '@/components/base_components/BaseButton.vue';
 import SvgIcon from './components/base_components/SvgIcon.vue';
@@ -16,7 +28,5 @@ app.component('SvgIcon', SvgIcon);
 app.component('BaseDialog', BaseDialog);
 app.component('Loader', Loader);
 
-
+app.use(i18n);
 app.mount('#app');
-
-
