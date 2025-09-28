b8b56c65bbb6a5e647ea9955f554397a331aac1b
o.smirnova <o.v.smirnova1987@gmail.com>
2025-09-28 22:55:12 +0300

    add component for language switching

---
+++ src/components/base_components/LocaleChanger.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { useI18n } from 'vue-i18n';
+import BaseButton from './BaseButton.vue';
+
+// access i18n instance
+const { locale } = useI18n({ useScope: 'global' });
+
+function changeLocale(lang) {
+  locale.value = lang;
+}
+const toggleLocale = () => {
+  locale.value = locale.value === 'en' ? 'de' : 'en';
+};
+</script>
+
+<template>
+  <div class="locale-switcher">
+    <BaseButton
+      class="btn btn-primary btn-primary--filled locale-switcher__btn"
+      v-if="locale === 'de'"
+      @click="changeLocale('en')"
+      >English</BaseButton
+    >
+    <BaseButton
+      class="btn btn-primary btn-primary--filled locale-switcher__btn"
+      v-else
+      @click="changeLocale('de')"
+    >
+      Deutsch
+    </BaseButton>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.locale-switcher {
+  &__btn {
+  }
+}
+</style>
