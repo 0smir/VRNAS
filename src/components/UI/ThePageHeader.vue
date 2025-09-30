c8efc36551a91418123bf5543be89bdaed714073
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-09-30 15:20:33 +0300

    add PageHeader template

---
+++ src/components/UI/ThePageHeader.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+const props = defineProps({
+  pageTitle: {
+    type: String,
+    required: false
+  }
+})
+</script>
+
+<template>
+  <header class="page__header">
+    <div v-if="pageTitle" class="container page__header-container">
+      <h1 class="h1 title title page__title">{{ pageTitle }}</h1>
+    </div>
+    <slot />
+  </header>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
