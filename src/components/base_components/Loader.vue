17c128e77c6b36553127f570434695e43a80da0c
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-09-30 18:22:31 +0300

    add styles for component

--- src/components/base_components/Loader.vue
+++ src/components/base_components/Loader.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <div className="loader__wrapper">
+  <div class="loader__wrapper">
+    <div class="loader">
       <SvgIcon class="icon icon--loader" name="loader" />
     </div>
   </div>
@@ -12,5 +12,11 @@
 </script>
 
 <style lang="scss" scoped>
+.loader__wrapper {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  flex: 1;
+}
 
 </style>
