54b4794843ef36390bb1ce967a6cc2abbf92cf02
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-09-30 15:14:18 +0300

    add new pages for blog will contain template list of articles and article page for article text

---
+++ src/components/pages/ArticleView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Article page
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/pages/BlogView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1 class="title page__title">Blog page</h1>
+    <p>with list of articles</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
