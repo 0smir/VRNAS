54536f17b1d9e70295e47de2ae48034d07a12fba
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-09-30 16:20:36 +0300

    add corrects to history params, add created pages views for Blog and Articles, add meta parameter with breadcrumbs array

--- src/router.js
+++ src/router.js
@@ -1,18 +1,84 @@
-import { createMemoryHistory, createRouter } from 'vue-router';
+import { createWebHistory, createRouter } from 'vue-router';
 
 import HomeView from '@/components/pages/HomeView.vue'
 import AboutView from '@/components/pages/AboutView.vue'
 import ServiceView from '@/components/pages/ServiceView.vue'
+import BlogView from '@/components/pages/BlogView.vue'
+import ArticleView from '@/components/pages/ArticleView.vue'
 
 const routes = [
-  { path: '/', component: HomeView },
-  { path: '/about', component: AboutView },
-  { path: '/service', component: ServiceView }
+  {
+    path: '/',
+    name: "home",
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: "about",
+    component: AboutView,
+    meta: {
+      breadcrumbs: [
+        {
+          label: "pages.home.title",
+          link: "/"
+        },
+        {
+          label: "pages.about.title"
+        }
+      ]
+    }
+  },
+  {
+    path: '/service',
+    name: "service",
+    component: ServiceView,
+    meta: {
+      breadcrumbs: [
+        {
+          label: "pages.home.title",
+          link: "/"
+        },
+        {
+          label: "pages.service.title"
+        }
+      ]
+    }
+  },
+  {
+    path: '/blog',
+    name: "blog",
+    component: BlogView,
+    meta: {
+      breadcrumbs: [{
+        label: "pages.home.title",
+        link: "/"
+      },
+      {
+        label: "pages.blog.title"
+      }],
+    }
+  },
+  {
+    path: '/blog/:id',
+    name: "article",
+    component: ArticleView,
+    meta: {
+      breadcrumbs: [
+        {
+          label: "pages.home.title",
+          link: "/"
+        },
+        {
+          label: "pages.blog.title",
+          link: '/blog'
+        }
+      ],
+    }
+  }
 ]
 
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes
 });
-
 export default router;
