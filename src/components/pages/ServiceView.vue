908735ac95058c161875b1a09b3ce9ebf7e34446
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-09-30 17:01:39 +0300

    add page header component with breadcrumbs

--- src/components/pages/ServiceView.vue
+++ src/components/pages/ServiceView.vue
@@ -1,17 +1,28 @@
-<template>
-    <div>
-        Service page
-        <div loader>
-            <loader />
-        </div>
-    </div>
-</template>
-
 <script setup>
+import PageHeader from '@/components/UI/ThePageHeader.vue';
+import Breadcrumbs from '@/components/UI/TheBreadcrumbs.vue';
+import { useI18n } from 'vue-i18n';
+import { useRoute } from 'vue-router';
 
+const { t } = useI18n();
+const router = useRoute();
+
+const breadcrumbsList = router.meta.breadcrumbs;
+console.log(breadcrumbsList);
 
 </script>
 
-<style lang="scss" scoped>
+<template>
+	<div class="container service__container">
+		<PageHeader :pageTitle="t('pages.service.title')" >
+			<Breadcrumbs :breadcrumbs="breadcrumbsList"/>
+		</PageHeader>
+	</div>
+	<div class="loader__wrapper">
+		<loader />
+	</div>
+</template>
+
+<style lang="scss">
 
 </style>
