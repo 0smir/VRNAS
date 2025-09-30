c56e1e00d49552abbafbbbf710c133c16ba39045
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-09-30 18:21:43 +0300

    add styles for page header component

--- src/components/UI/ThePageHeader.vue
+++ src/components/UI/ThePageHeader.vue
@@ -9,7 +9,7 @@ const props = defineProps({
 </script>
 
 <template>
-  <header class="page__header">
+  <header class="page__header highlighted">
     <div v-if="pageTitle" class="container page__header-container">
       <h1 class="h1 title title page__title">{{ pageTitle }}</h1>
     </div>
@@ -20,5 +20,31 @@ const props = defineProps({
 
 
 <style lang="scss" scoped>
-
+  .page__header {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: min(400px, 40dvh);
+    border-bottom: 1px solid;
+    border-image: $border_with_gradient;
+    margin-bottom: 40px;
+  }
+  .page__title{
+    text-align: center;
+  }
+  .highlighted{
+    &::after {
+      position: absolute;
+      content: '';  
+      display: block;
+      inset: 0;
+      z-index: -1;
+      filter: blur(20px);
+      background-image: url('@/assets/images/light_bg.png'),  url('@/assets/images/light_bg.png');
+      background-repeat: no-repeat no-repeat;
+      background-position:  50% 100%,  50% 0%;
+      background-size: contain, contain;
+    }
+  }
 </style>
