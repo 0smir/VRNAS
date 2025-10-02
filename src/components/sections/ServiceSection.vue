cdaf3fc010067b08ec62fd5607067ed8e9f3a83f
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-10-02 16:14:40 +0300

    add styles for section service

--- src/components/sections/ServiceSection.vue
+++ src/components/sections/ServiceSection.vue
@@ -14,13 +14,13 @@ const servisesList = SERVICES_INFO.services_list;
 
 <template>
   <header class="section__header">
-    <span class="section__subtitle">{{ sectionSubTitle }}</span>
+    <span class="subtitle section__subtitle">{{ sectionSubTitle }}</span>
     <h2 class="title section__title">{{ sectionTitle }}</h2>
     <div class="section__description">
       <p class="text">{{ sectiondescription }}</p>
     </div>
   </header>
-  <div class="servises__wrapper">
+  <div class="service__wrapper">
     <ul class="service__list">
       <li class="service__item" v-for="servise_item in servisesList">
         <ServiceItem :key="servise_item.id" :serviceData="servise_item" />
@@ -34,7 +34,6 @@ const servisesList = SERVICES_INFO.services_list;
   &__header {
     display: grid;
     margin-bottom: 20px;
-
     @media (min-width: $md) {
       grid-auto-flow: column;
       grid-template-columns: repeat(2, 1fr);
@@ -45,27 +44,42 @@ const servisesList = SERVICES_INFO.services_list;
     }
   }
   &__subtitle {
-    grid-area: 'subtitle';
+    justify-self: start;
+    @media (min-width: $md) {
+      grid-area: subtitle;
+    }
   }
   &__title {
-    grid-area: 'title';
+    margin-bottom: 20px;
+    @media (min-width: $md) {
+      grid-area: title;
+      margin-bottom: 0;
+    }
   }
   &__description {
-    grid-area: 'description';
+    @media (min-width: $md) {
+      grid-area: description;
+    }
   }
 }
 
 .service {
+  &__wrapper {
+    background-image: url('@/assets/images/assets.svg');
+    background-repeat: no-repeat;
+    background-position: 50% 50%;
+    @media (min-width: $md) {
+      background-image: none;
+    }
+  }
   &__list {
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(380px, 1fr));
     grid-template-rows: repeat(auto-fit, minmax(290px, 1fr));
     list-style: none;
-
+    gap: 20px;
     @media (min-width: $md) {
       grid-template-columns: repeat(auto-fit, minmax(max(380px, 30%), 1fr));
-      // justify-content: space-between;
-      gap: 20px;
     }
   }
 }
